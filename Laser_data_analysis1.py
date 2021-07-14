Laser_data_analysis1.py corrected to consider the area in which the spatial laplacian change sign, but this is not enough and needs further work. MASTU data process modified to first do filtering, crop/rotation and conversion and then save that, and doing the binning only afterwards.

Laser_data_analysis1.py
@@ -8,11 +8,14 @@ exec(open("/home/ffederic/work/analysis_scripts/scripts/preamble_import_pc.py").
 # #this is if working in batch, use predefined NOT visual printer
 # exec(open("/home/ffederic/work/analysis_scripts/scripts/preamble_import_batch.py").read())
 
+# to show the line where it fails
+import sys, traceback, logging
+logging.basicConfig(level=logging.ERROR)
 
 #this is for importing all the variables names and which are the files
 exec(open("/home/ffederic/work/analysis_scripts/scripts/preamble_indexing.py").read())
 from multiprocessing import Pool,cpu_count,current_process,set_start_method,get_context,Semaphore
-number_cpu_available = 5
+number_cpu_available = 8
 # number_cpu_available = cpu_count()
 
 
@@ -54,8 +57,8 @@ else:	# automatic collection of parameters
 	all_power_interpolator = []
 	all_foil_position_dict = []
 	for name in cases_to_include:
-		if collection_of_records[name]['focus_status'][0] != 'fully_defocused':
-			continue
+		# if collection_of_records[name]['focus_status'][0] == 'fully_defocused':
+		# 	continue
 		all_case_ID.extend([name] * len(collection_of_records[name]['path_files_laser']))
 		all_path_reference_frames.extend(collection_of_records[name]['reference_clear'])
 		all_laser_to_analyse.extend(collection_of_records[name]['path_files_laser'])
@@ -87,8 +90,8 @@ parameters_available = f[0]
 parameters_available_int_time = []
 parameters_available_framerate = []
 for path in parameters_available:
-	parameters_available_int_time.append(np.float(path[:path.find('ms')]))
-	parameters_available_framerate.append(np.float(path[path.find('ms')+2:path.find('Hz')]))
+	parameters_available_int_time.append(float(path[:path.find('ms')]))
+	parameters_available_framerate.append(float(path[path.find('ms')+2:path.find('Hz')]))
 parameters_available_int_time = np.array(parameters_available_int_time)
 parameters_available_framerate = np.array(parameters_available_framerate)
 
@@ -107,10 +110,14 @@ parameters_available_framerate = np.array(parameters_available_framerate)
 
 # example good properties that I use to see how detected power changes with sampling area
 sample_properties = dict([])
-sample_properties['thickness'] = 3.2*1e-6
+# sample_properties['thickness'] = 3.2*1e-6
+# sample_properties['emissivity'] = 1
+# sample_properties['diffusivity'] = 1.32*1e-5
+sample_properties['thickness'] = 2.093616658223934e-06
 sample_properties['emissivity'] = 1
-sample_properties['diffusivity'] = 1.32*1e-5
-
+sample_properties['diffusivity'] = 1.03*1e-5
+# BBtreshold = 0.06
+BBtreshold = 0.13
 
 def function_a(index):
 	from uncertainties.unumpy import nominal_values,std_devs
@@ -255,7 +262,7 @@ def function_a(index):
 		foilhorizw=0.09	# m
 		foilvertw=0.07	# m
 		foilhorizwpixel = foil_position_dict['foilhorizwpixel']
-		foilvertwpixel=np.int((foilhorizwpixel*foilvertw)//foilhorizw)
+		foilvertwpixel=int((foilhorizwpixel*foilvertw)//foilhorizw)
 		r=((foilhorizwpixel**2+foilvertwpixel**2)**0.5)/2  # HALF DIAGONAL
 		a=foilvertwpixel/np.cos(foilrot)
 		tgalpha=np.tan(foilrot)
@@ -295,10 +302,10 @@ def function_a(index):
 		precisionincrease=10
 		dummy=np.ones(np.multiply(np.shape(testrot),precisionincrease))
 		dummy[foilcenter[1]*precisionincrease,foilcenter[0]*precisionincrease]=2
-		dummy[np.int(foily[0]*precisionincrease),np.int(foilx[0]*precisionincrease)]=3
-		dummy[np.int(foily[1]*precisionincrease),np.int(foilx[1]*precisionincrease)]=4
-		dummy[np.int(foily[2]*precisionincrease),np.int(foilx[2]*precisionincrease)]=5
-		dummy[np.int(foily[3]*precisionincrease),np.int(foilx[3]*precisionincrease)]=6
+		dummy[int(foily[0]*precisionincrease),int(foilx[0]*precisionincrease)]=3
+		dummy[int(foily[1]*precisionincrease),int(foilx[1]*precisionincrease)]=4
+		dummy[int(foily[2]*precisionincrease),int(foilx[2]*precisionincrease)]=5
+		dummy[int(foily[3]*precisionincrease),int(foilx[3]*precisionincrease)]=6
 		dummy2=rotate(dummy,foilrotdeg,axes=(-1,-2),order=0)
 		foilcenterrot=(np.rint([np.where(dummy2==2)[1][0]/precisionincrease,np.where(dummy2==2)[0][0]/precisionincrease])).astype('int')
 		foilxrot=(np.rint([np.where(dummy2==3)[1][0]/precisionincrease,np.where(dummy2==4)[1][0]/precisionincrease,np.where(dummy2==5)[1][0]/precisionincrease,np.where(dummy2==6)[1][0]/precisionincrease,np.where(dummy2==3)[1][0]/precisionincrease])).astype('int')
@@ -425,6 +432,7 @@ def function_a(index):
 		# I subtract the background to the counts and add back the average of the background counts
 		# NO!! I cannot do this either, because the counts to temp coefficients are fuilt in with the disuniformity, and non't work if I remove it before hand.
 
+	laser_dict.allow_pickle=True
 	temp1 = laser_dict['laser_temperature_minus_background_median']	# this is NOT minus_background, that is a relic of what I did before, but I keep it not to recreate all .npz, same for the next3
 	temp2 = laser_dict['laser_temperature_minus_background_minus_median_downgraded']
 	temp3 = laser_dict['laser_temperature_minus_background_std_median']
@@ -488,8 +496,8 @@ def function_a(index):
 		laser_temperature_std_minus_background_rot[np.logical_and(laser_temperature_minus_background_rot<np.nanmin(laser_temperature_minus_background_full),laser_temperature_minus_background_rot>np.nanmax(laser_temperature_minus_background_full))]=0
 		laser_temperature_minus_background_rot*=out_of_ROI_mask
 		laser_temperature_minus_background_rot[np.logical_and(laser_temperature_minus_background_rot<np.nanmin(laser_temperature_minus_background_full),laser_temperature_minus_background_rot>np.nanmax(laser_temperature_minus_background_full))]=0
-	laser_temperature_minus_background_crop=laser_temperature_minus_background_rot[:,foildw:foilup,foillx:foilrx]
-	laser_temperature_std_minus_background_crop=laser_temperature_std_minus_background_rot[:,foildw:foilup,foillx:foilrx]
+	laser_temperature_minus_background_crop=laser_temperature_minus_background_rot[:,foildw:foilup,foillx:foilrx].astype(np.float32)
+	laser_temperature_std_minus_background_crop=laser_temperature_std_minus_background_rot[:,foildw:foilup,foillx:foilrx].astype(np.float32)
 	nan_ROI_mask = np.isfinite(np.nanmedian(laser_temperature_minus_background_crop[:10],axis=0))
 
 	reference_background_temperature_rot=rotate(reference_background_temperature_no_dead_pixels,foilrotdeg,axes=(-1,-2))
@@ -500,9 +508,9 @@ def function_a(index):
 		reference_background_temperature_rot*=out_of_ROI_mask
 		reference_background_temperature_rot[np.logical_and(reference_background_temperature_rot<np.nanmin(reference_background_temperature_no_dead_pixels),reference_background_temperature_rot>np.nanmax(reference_background_temperature_no_dead_pixels))]=np.nanmean(reference_background_temperature_no_dead_pixels)
 	reference_background_temperature_crop=reference_background_temperature_rot[:,foildw:foilup,foillx:foilrx]
-	reference_background_temperature_crop = np.nanmean(reference_background_temperature_crop,axis=0)
+	reference_background_temperature_crop = np.nanmean(reference_background_temperature_crop,axis=0).astype(np.float32)
 	reference_background_temperature_std_crop=reference_background_temperature_std_rot[:,foildw:foilup,foillx:foilrx]
-	reference_background_temperature_std_crop = np.nanmean(reference_background_temperature_std_crop,axis=0)
+	reference_background_temperature_std_crop = np.nanmean(reference_background_temperature_std_crop,axis=0).astype(np.float32)
 
 	del laser_temperature_minus_background_rot,laser_temperature_std_minus_background_rot,reference_background_temperature_no_dead_pixels,reference_background_temperature_std_no_dead_pixels
 
@@ -740,19 +748,71 @@ def function_a(index):
 	power_vs_space_sampling = power_vs_space_sampling_explorer()
 
 	dr = (power_vs_space_sampling['all_dr'])[nominal_values(power_vs_space_sampling['fitted_powers'][:,1][power_vs_space_sampling['all_dr']<=dr]).argmax()]
+
+	if focus_status == 'focused':
+		dr_total_power_minimum = 5
+		# dr_total_power = 17
+	elif focus_status == 'partially_defocused':
+		dr_total_power_minimum = 7
+		# dr_total_power = 20
+	elif focus_status == 'fully_defocused':
+		dr_total_power_minimum = 10
+		# dr_total_power = 25
+	else:
+		print('focus type '+focus_status+" unknown, used 'fully_defocused' settings")
+		# dr_total_power = 16
+		dr_total_power_minimum = 25
+
+	limit = 110
+	test=0
+	while True:
+		select1 = (((horizontal_coord-horizontal_loc)**2 + (vertical_coord-vertical_loc)**2)>(limit)**2)[1:-1,1:-1]
+		if np.nansum(nan_ROI_mask[1:-1,1:-1][select1])>300:
+			break
+		else:
+			limit -= 10
+	fig, ax = plt.subplots( 2,1,figsize=(10, 14), squeeze=False, sharex=True)
+	time_averaged_BBrad_over_duty = np.nanmean(temp_BBrad[:int(len(temp_BBrad)//frames_for_one_pulse*frames_for_one_pulse)],axis=0)
+	test = []
+	for value in np.arange(5,limit-10):
+		select2 = np.logical_and( (((horizontal_coord-horizontal_loc)**2 + (vertical_coord-vertical_loc)**2)<=(value+2)**2)[1:-1,1:-1] , (((horizontal_coord-horizontal_loc)**2 + (vertical_coord-vertical_loc)**2)>value**2)[1:-1,1:-1] )
+		test.append(np.nanmean(time_averaged_BBrad_over_duty[select2])-np.nanmean(time_averaged_BBrad_over_duty[select1]))
+	dr_total_power_BB = np.arange(5,limit-10)[np.abs(np.array(test)-BBtreshold).argmin()]
+	ax[0,0].plot(np.arange(5,limit-10)*dx,test)
+	ax[0,0].axhline(y=BBtreshold,linestyle='--',color='k',label='treshold')
+	ax[0,0].axvline(x=dr_total_power_BB*dx,linestyle='--',color='r',label='detected')
+	ax[0,0].axvline(x=dr_total_power_minimum*dx,linestyle='--',color='b',label='minimum')
+	ax[0,0].set_ylim(top=min(np.nanmax(test),BBtreshold*5),bottom=np.nanmin(test))
+	ax[0,0].set_xlim(left=0)
+	ax[0,0].set_ylabel('black body')
+	ax[0,0].legend(loc='best', fontsize='x-small')
+	ax[0,0].grid()
+	temp = np.nanmax(laser_temperature_minus_background_crop[1:-1],axis=(1,2))
+	time_averaged_diffusion_over_duty = np.nanmean(temp_diffusion[temp>np.sort(temp)[-int(len(temp)*experimental_laser_duty)]],axis=0)
+	test = []
+	for value in np.arange(5,limit-10):
+		select1 = (((horizontal_coord-horizontal_loc)**2 + (vertical_coord-vertical_loc)**2)>(value)**2)[1:-1,1:-1]
+		select2 = np.logical_and( (((horizontal_coord-horizontal_loc)**2 + (vertical_coord-vertical_loc)**2)<=(value+4)**2)[1:-1,1:-1] , (((horizontal_coord-horizontal_loc)**2 + (vertical_coord-vertical_loc)**2)>value**2)[1:-1,1:-1] )
+		test.append(np.nanmean(time_averaged_diffusion_over_duty[select1]))
+	dr_total_power_diff = np.arange(5,limit-10)[np.abs((test[:40]+2*np.max(np.abs(test[40:])))).argmin()]
+	ax[1,0].plot(np.arange(5,limit-10)*dx,test)
+	ax[1,0].axhline(y=-2*np.nanmax(np.abs(test[40:])),linestyle='--',color='k')
+	ax[1,0].axvline(x=dr_total_power_diff*dx,linestyle='--',color='r')
+	ax[1,0].axvline(x=dr_total_power_minimum*dx,linestyle='--',color='b')
+	ax[1,0].set_ylim(bottom=-4*np.nanmax(np.abs(test[40:])),top=np.nanmax(test))
+	ax[1,0].set_xlim(left=0)
+	ax[1,0].set_ylabel('diffusion')
+	ax[1,0].set_xlabel('radious of power sum [mm]')
+	ax[1,0].grid()
+	dr_total_power = np.max([dr_total_power_diff,dr_total_power_minimum,dr_total_power_BB])
 	# temp = np.logical_and(power_vs_space_sampling['all_dr']>dr*1.2,power_vs_space_sampling['all_dr']<np.max(power_vs_space_sampling['all_dr']))
 	# dr_total_power = (power_vs_space_sampling['all_dr'][temp])[nominal_values(power_vs_space_sampling['fitted_powers'][:,1][temp]).argmin()]
 	# dr_total_power = dr*2
-	if focus_status == 'focused':
-		dr_total_power = 5
-	elif focus_status == 'partially_defocused':
-		dr_total_power = 7
-	elif focus_status == 'fully_defocused':
-		dr_total_power = 10
-	else:
-		print('focus type '+focus_status+" unknown, used 'fully_defocused' settings")
-		dr_total_power = 16
 	select = (((horizontal_coord-horizontal_loc)**2 + (vertical_coord-vertical_loc)**2)<=dr_total_power**2)[1:-1,1:-1]
+	fig.suptitle(preamble_4_prints+'Search for power sum size in '+laser_to_analyse+'\nfound %.3gmm' %(dr_total_power*dx))
+	figure_index+=1
+	plt.savefig(path_to_save_figures+laser_to_analyse[-6:] + path_to_save_figures2 + 'FIG'+str(figure_index)+'.eps', bbox_inches='tight')
+	plt.close('all')
 
 	def power_vs_time_sampling_explorer():
 		from uncertainties import ufloat
@@ -821,7 +881,7 @@ def function_a(index):
 	timevariation[:,nan_ROI_mask[1:-1,1:-1]] = (temp_timevariation[:,nan_ROI_mask[1:-1,1:-1]]*foilthicknessscaled[nan_ROI_mask[1:-1,1:-1]]*reciprdiffusivityscaled[nan_ROI_mask[1:-1,1:-1]]).astype(np.float32)
 	timevariation_std = np.ones_like(temp_BBrad).astype(np.float32)*np.nan
 	timevariation_std[:,nan_ROI_mask[1:-1,1:-1]] = (temp_timevariation_std[:,nan_ROI_mask[1:-1,1:-1]]*foilthicknessscaled[nan_ROI_mask[1:-1,1:-1]]*reciprdiffusivityscaled[nan_ROI_mask[1:-1,1:-1]]).astype(np.float32)
-	del temp_BBrad,temp_BBrad_std,temp_diffusion,temp_diffusion_std,temp_timevariation,temp_timevariation_std
+	del temp_BBrad_std,temp_diffusion_std,temp_timevariation,temp_timevariation_std
 	powernoback = (diffusion + timevariation + BBrad).astype(np.float32)
 	powernoback_std = np.ones_like(powernoback)*np.nan
 	powernoback_std[:,nan_ROI_mask[1:-1,1:-1]] = ((diffusion_std[:,nan_ROI_mask[1:-1,1:-1]]**2 + timevariation_std[:,nan_ROI_mask[1:-1,1:-1]]**2 + BBrad_std[:,nan_ROI_mask[1:-1,1:-1]]**2)**0.5).astype(np.float32)
@@ -862,6 +922,42 @@ def function_a(index):
 	figure_index+=1
 	plt.savefig(path_to_save_figures+laser_to_analyse[-6:] + path_to_save_figures2 + 'FIG'+str(figure_index)+'.eps', bbox_inches='tight')
 	plt.close('all')
+
+	plt.figure(figsize=(20, 10))
+	time_averaged_diffusion_over_duty = np.nanmean(temp_diffusion[temp>np.sort(temp)[-int(len(temp)*experimental_laser_duty)]],axis=0)
+	plt.pcolor(h_coordinates,v_coordinates,generic_filter(time_averaged_diffusion_over_duty,np.mean,size=[3,3]),cmap='rainbow',vmax=0)#vmin=np.nanmin(time_averaged_diff_over_duty[select]),vmax=np.nanmax(time_averaged_diff_over_duty[select]))
+	plt.colorbar().set_label('Power [W/m2]')
+	# plt.errorbar(horizontal_loc*dx,vertical_loc*dx,xerr=dhorizontal_loc/2*dx,yerr=dvertical_loc/2*dx,color='k',linestyle='--')
+	plt.plot((horizontal_loc + np.arange(-dr,+dr+dr/10,dr/10))*dx,(vertical_loc + np.abs(dr**2-np.arange(-dr,+dr+dr/10,dr/10)**2)**0.5)*dx,'r--',label='found laser size')
+	plt.plot((horizontal_loc + np.arange(-dr,+dr+dr/10,dr/10))*dx,(vertical_loc - np.abs(dr**2-np.arange(-dr,+dr+dr/10,dr/10)**2)**0.5)*dx,'r--')
+	plt.plot((horizontal_loc + np.arange(-dr_total_power,+dr_total_power+dr_total_power/10,dr_total_power/10))*dx,(vertical_loc + np.abs(dr_total_power**2-np.arange(-dr_total_power,+dr_total_power+dr_total_power/10,dr_total_power/10)**2)**0.5)*dx,'k--',label='area accounted for sum')
+	plt.plot((horizontal_loc + np.arange(-dr_total_power,+dr_total_power+dr_total_power/10,dr_total_power/10))*dx,(vertical_loc - np.abs(dr_total_power**2-np.arange(-dr_total_power,+dr_total_power+dr_total_power/10,dr_total_power/10)**2)**0.5)*dx,'k--')
+	plt.title(preamble_4_prints+'Diffusion component shape in '+laser_to_analyse+'\n foil size '+str([foilhorizwpixel,foilvertwpixel])+'pixels, [%.3g,%.3g]mm\n laser located at [%.3g,%.3g]mm laser radious %.3gmm' %(foilhorizwpixel*dx*1e3,foilvertwpixel*dx*1e3,horizontal_loc*dx*1e3,vertical_loc*dx*1e3,dr*dx*1e3))
+	plt.xlabel('Horizontal axis [mm]')
+	plt.ylabel('Vertical axis [mm]')
+	plt.legend(loc='best', fontsize='small')
+	figure_index+=1
+	plt.savefig(path_to_save_figures+laser_to_analyse[-6:] + path_to_save_figures2 + 'FIG'+str(figure_index)+'.eps', bbox_inches='tight')
+	plt.close('all')
+	del temp_diffusion
+
+	plt.figure(figsize=(20, 10))
+	time_averaged_BBrad_over_duty = np.nanmean(temp_BBrad[:int(len(temp_BBrad)//frames_for_one_pulse*frames_for_one_pulse)],axis=0)
+	plt.pcolor(h_coordinates,v_coordinates,time_averaged_BBrad_over_duty,cmap='rainbow',vmax=BBtreshold*5)
+	plt.colorbar().set_label('Power [W/m2]')
+	# plt.errorbar(horizontal_loc*dx,vertical_loc*dx,xerr=dhorizontal_loc/2*dx,yerr=dvertical_loc/2*dx,color='k',linestyle='--')
+	plt.plot((horizontal_loc + np.arange(-dr,+dr+dr/10,dr/10))*dx,(vertical_loc + np.abs(dr**2-np.arange(-dr,+dr+dr/10,dr/10)**2)**0.5)*dx,'r--',label='found laser size')
+	plt.plot((horizontal_loc + np.arange(-dr,+dr+dr/10,dr/10))*dx,(vertical_loc - np.abs(dr**2-np.arange(-dr,+dr+dr/10,dr/10)**2)**0.5)*dx,'r--')
+	plt.plot((horizontal_loc + np.arange(-dr_total_power,+dr_total_power+dr_total_power/10,dr_total_power/10))*dx,(vertical_loc + np.abs(dr_total_power**2-np.arange(-dr_total_power,+dr_total_power+dr_total_power/10,dr_total_power/10)**2)**0.5)*dx,'k--',label='area accounted for sum')
+	plt.plot((horizontal_loc + np.arange(-dr_total_power,+dr_total_power+dr_total_power/10,dr_total_power/10))*dx,(vertical_loc - np.abs(dr_total_power**2-np.arange(-dr_total_power,+dr_total_power+dr_total_power/10,dr_total_power/10)**2)**0.5)*dx,'k--')
+	plt.title(preamble_4_prints+'BB component shape in '+laser_to_analyse+'\n foil size '+str([foilhorizwpixel,foilvertwpixel])+'pixels, [%.3g,%.3g]mm\n laser located at [%.3g,%.3g]mm laser radious %.3gmm' %(foilhorizwpixel*dx*1e3,foilvertwpixel*dx*1e3,horizontal_loc*dx*1e3,vertical_loc*dx*1e3,dr*dx*1e3))
+	plt.xlabel('Horizontal axis [mm]')
+	plt.ylabel('Vertical axis [mm]')
+	plt.legend(loc='best', fontsize='small')
+	figure_index+=1
+	plt.savefig(path_to_save_figures+laser_to_analyse[-6:] + path_to_save_figures2 + 'FIG'+str(figure_index)+'.eps', bbox_inches='tight')
+	plt.close('all')
+	del temp_BBrad
 
 
 	totalpower_filtered_1 = generic_filter(totalpower[:int(len(totalpower)//frames_for_one_pulse*frames_for_one_pulse)],np.mean,size=[max(1,int(laser_framerate/experimental_laser_frequency*experimental_laser_duty/15//2*2+1))])
@@ -991,6 +1087,7 @@ def function_a(index):
 	full_saved_file_dict['power_vs_space_sampling']=power_vs_space_sampling
 	full_saved_file_dict['power_vs_time_sampling']=power_vs_time_sampling
 	full_saved_file_dict['laser_input_power']=laser_to_analyse_power
+	full_saved_file_dict['time_of_experiment'] = time_of_experiment
 	np.savez_compressed(laser_to_analyse,**full_saved_file_dict)
 	print('FINISHED '+laser_to_analyse)
 	return 0
@@ -1008,7 +1105,11 @@ def function_a(index):
 # 	pool.terminate()
 # 	del pool
 for index in range(len(all_laser_to_analyse)):
-	function_a(index)
+	try:
+		function_a(index)
+	except Exception as e:
+		print('index '+str(index)+' failed')
+		logging.exception('with error: ' + str(e))
 
 
 #

Laser_data_analysis3.py
@@ -1,6 +1,6 @@
 # Created 28/06/2021
 # Fabio Federici
-# I start from Laser_data_analysis3, but I want to start from the steady state, that gives me emissivity and diffusivity
+# I start from Laser_data_analysis2, but I want to start from the steady state, that gives me emissivity and diffusivity
 
 #this is if working on a pc, use pc printer
 exec(open("/home/ffederic/work/analysis_scripts/scripts/preamble_import_pc.py").read())
@@ -64,16 +64,16 @@ index1=0
 index2=0
 index3=0
 for array in f:
-	x=np.float(array[2])*dx+np.random.random()/4*dx
-	y=np.float(array[4])*dx+np.random.random()/4*dx
+	x=float(array[2])*dx+np.random.random()/4*dx
+	y=float(array[4])*dx+np.random.random()/4*dx
 	if array[0]=='fully_defocused':
-		plt.plot(x,y,'o',fillstyle='none',color=color[index1],label='full def'+' FR%.5gHz ' %(np.float(array[-1])) + array[1],markersize=5+index1*2)
+		plt.plot(x,y,'o',fillstyle='none',color=color[index1],label='full def'+' FR%.5gHz ' %(float(array[-1])) + array[1],markersize=5+index1*2)
 		index1+=1
 	elif array[0]=='partially_defocused':
-		plt.plot(x,y,'v',fillstyle='none',color=color[index2],label='part def'+' FR%.5gHz ' %(np.float(array[-1])) + array[1],markersize=5+index2*2)
+		plt.plot(x,y,'v',fillstyle='none',color=color[index2],label='part def'+' FR%.5gHz ' %(float(array[-1])) + array[1],markersize=5+index2*2)
 		index2+=1
 	else:
-		plt.plot(x,y,'+',color=color[index3],label='focus'+' FR%.5gHz ' %(np.float(array[-1])) + array[1],markersize=5+index3*2)
+		plt.plot(x,y,'+',color=color[index3],label='focus'+' FR%.5gHz ' %(float(array[-1])) + array[1],markersize=5+index3*2)
 		index3+=1
 	plt.annotate(array[1][-2:],(x+1e-4,y+1e-4),fontsize=10)
 plt.plot([0,foilhorizw,foilhorizw,0,0],[0,0,foilvertw,foilvertw,0],'k')
@@ -177,7 +177,7 @@ for cases_to_include in all_cases_to_include:
 			partial_diffusion_std = laser_dict['partial_diffusion_std']
 			partial_timevariation = laser_dict['partial_timevariation']
 			partial_timevariation_std = laser_dict['partial_timevariation_std']
-			time_of_experiment = laser_dict['time_of_measurement']	# microseconds
+			time_of_experiment = laser_dict['time_of_experiment']	# microseconds
 			if np.diff(time_of_experiment).max()>np.median(np.diff(time_of_experiment))*1.1:
 				hole_pos = np.diff(time_of_experiment).argmax()
 				if hole_pos<len(time_of_experiment)/2:
@@ -248,6 +248,7 @@ for cases_to_include in all_cases_to_include:
 		print([search_emissivity,search_thickness])
 		search_diffusivity = Ptthermaldiffusivity
 		all_fitted_power = []
+		all_fitted_zero_power = []
 		for index in range(len(all_laser_to_analyse_power_end)):
 			time_of_experiment = all_time_of_experiment[index]
 			partial_BBrad = all_partial_BBrad[index]
@@ -265,9 +266,10 @@ for cases_to_include in all_cases_to_include:
 			# BBrad_std = partial_BBrad_std * 1
 			diffusion = partial_diffusion * search_thickness
 			# diffusion_std = partial_diffusion_std * search_thickness
-			timevariation = (1/search_diffusivity)*search_thickness * partial_timevariation
+			# timevariation = (1/search_diffusivity)*search_thickness * partial_timevariation
 			# timevariation_std = (1/search_diffusivity)*search_thickness * partial_timevariation_std
-			powernoback = diffusion + timevariation + BBrad
+			# powernoback = diffusion + timevariation + BBrad
+			powernoback = diffusion + BBrad
 			# powernoback_std = (diffusion_std**2 + timevariation_std**2 + BBrad_std**2)**0.5
 			frames_for_one_pulse = int(laser_framerate/experimental_laser_frequency)
 			time_ON_after_SS = int(frames_for_one_pulse*experimental_laser_duty - minimum_ON_period*laser_framerate)
@@ -282,15 +284,17 @@ for cases_to_include in all_cases_to_include:
 			# plt.plot(time_of_experiment[1:-1],totalpower_filtered_1)
 			# plt.pause(0.01)
 
-			all_fitted_power.append(totalpower_filtered_1.max())
-		all_fitted_power = np.array(all_fitted_power)/(all_laser_to_analyse_power_end*power_reduction_window)
+			all_fitted_power.append(totalpower_filtered_1.max()-totalpower_filtered_1.min())
+			all_fitted_zero_power.append(totalpower_filtered_1.min())
+		all_fitted_power = np.concatenate([np.array(all_fitted_power)/(all_laser_to_analyse_power_end*power_reduction_window),np.array(all_fitted_zero_power)/(all_laser_to_analyse_power_end*power_reduction_window)])
 		return all_fitted_power
 
-	x = np.arange(len(all_laser_to_analyse_power_end))
-	y = np.ones_like(all_laser_to_analyse_power_end)
+	x = np.arange(len(all_laser_to_analyse_power_end)*2)
+	y = np.concatenate([np.ones_like(all_laser_to_analyse_power_end),np.zeros_like(all_laser_to_analyse_power_end)])
 	# weigth = np.array([np.ones_like(all_laser_to_analyse_power_end)*1,np.ones_like(all_laser_to_analyse_power_end)*0.1,np.ones_like(all_laser_to_analyse_power_end)*0.001*sharpness_degradation_high_frequency]).T.flatten()
 	# sigma = np.ones_like(y)
 	sigma = np.abs(all_laser_to_analyse_power_end-all_laser_to_analyse_power_end.max()) + all_laser_to_analyse_power_end.max()
+	sigma = np.concatenate([sigma,sigma])
 	bds = [[0.7,0.1*2.5e-6],[1,10*2.5e-6]]
 	guess=[1,2.5e-6]
 	fit = curve_fit(calculate_laser_power_given_parameters_1, x, y, sigma=sigma, p0=guess,bounds=bds,maxfev=int(1e6),verbose=2,ftol=1e-12,xtol=1e-14,gtol=1e-12)
@@ -298,7 +302,7 @@ for cases_to_include in all_cases_to_include:
 	guess_best_power = calculate_laser_power_given_parameters_1(1,*guess)
 	emissivity_first_stage, thickness_first_stage = fit[0]
 	coefficients_first_stage.append(fit[0])
-	all_sharpness_first.append(np.nanmean(best_sharpness))
+	# all_sharpness_first.append(np.nanmean(best_sharpness))
 
 	minimum_ON_period = minimum_ON_period_list[1]
 
@@ -348,7 +352,7 @@ for cases_to_include in all_cases_to_include:
 			partial_diffusion_std = laser_dict['partial_diffusion_std']
 			partial_timevariation = laser_dict['partial_timevariation']
 			partial_timevariation_std = laser_dict['partial_timevariation_std']
-			time_of_experiment = laser_dict['time_of_measurement']	# microseconds
+			time_of_experiment = laser_dict['time_of_experiment']	# microseconds
 			laser_framerate = laser_dict['FrameRate']	# Hz
 			laser_location = laser_dict['laser_location']
 			success_read = True
@@ -493,7 +497,9 @@ for cases_to_include in all_cases_to_include:
 	fit = curve_fit(calculate_laser_power_given_parameters, x, y, sigma=weigth, p0=guess,bounds=bds,maxfev=int(1e6),verbose=2,ftol=1e-12,xtol=1e-14,gtol=1e-12)
 	best_sharpness = calculate_laser_power_given_parameters(1,*fit[0])
 	diffusivity_second_stage = fit[0][0]
-	diffusivity_second_stage = 1.03e-5	# this seems to work better. I'm fixing now the experiments in which there are breaks in time that mess up things.
+	# diffusivity_second_stage = 1.03e-5	# this seems to work better. I'm fixing now the experiments in which there are breaks in time that mess up things.
+	# # when that is done I hope that this automatic procedure will give me around the value I found manually
+	diffusivity_second_stage = 4.2e-5	# this seems to work better. I'm fixing now the experiments in which there are breaks in time that mess up things.
 	# when that is done I hope that this automatic procedure will give me around the value I found manually
 
 	for index in np.arange(len(all_laser_to_analyse_power_end)):
@@ -620,7 +626,7 @@ for cases_to_include in all_cases_to_include:
 		plt.xlabel('time [s]')
 		plt.ylabel('power [W]')
 		plt.grid()
-		plt.title(laser_to_analyse+' in '+case_ID+'\nInput '+focus_status+', spot location '+ str([int(laser_location[0]),int(laser_location[2])]) +' power %.3gW, freq %.3gHz, duty%.3g\nHigh Power=%.3g+/-%.3gW, Low Power=%.3g+/-%.3gW, sharpness=%.3g' %(laser_to_analyse_power_end*defocused_to_focused_power,experimental_laser_frequency,experimental_laser_duty,fitted_power_2[2],fitted_power_2[3],fitted_power_2[0],fitted_power_2[1],sharpness_indicator))
+		plt.title(laser_to_analyse+' in '+case_ID+'\nInput '+focus_status+', spot location '+ str([int(laser_location[0]),int(laser_location[2])]) +' power %.3gW, freq %.3gHz, duty%.3g\nHigh Power=%.3g+/-%.3gW, Low Power=%.3g+/-%.3gW, sharpness=%.3g' %(laser_to_analyse_power_end*defocused_to_focused_power*power_reduction_window,experimental_laser_frequency,experimental_laser_duty,fitted_power_2[2],fitted_power_2[3],fitted_power_2[0],fitted_power_2[1],sharpness_indicator))
 		# plt.pause(0.01)
 		figure_index+=1
 		plt.savefig(path_where_to_save_everything_int + 'example_' + str(figure_index) +'.eps', bbox_inches='tight')

MASTU_pulse_process_launcer.py
@@ -40,47 +40,31 @@ parameters_available_int_time = np.array(parameters_available_int_time)
 parameters_available_framerate = np.array(parameters_available_framerate)
 
 
-path = '/home/ffederic/work/irvb/MAST-U/'
-# to_do = ['2021-05-18','2021-05-19','2021-05-20','2021-05-21','2021-05-25','2021-05-26','2021-05-27','2021-05-28','2021-06-02','2021-06-03','2021-06-04','2021-06-15','2021-06-16','2021-06-17','2021-06-18','2021-06-22','2021-06-23','2021-06-24','2021-06-25','2021-06-29','2021-06-30','2021-07-01','2021-07-06','2021-07-07']
-to_do = ['2021-07-06']
-to_do = np.flip(to_do,axis=0)
-# to_do = ['2021-06-03']
-# path = '/home/ffederic/work/irvb/MAST-U/preliminaly_shots/'
-# to_do = ['2021-05-13','2021-05-12','2021-04-28','2021-04-29','2021-04-30']
-
 seconds_for_bad_pixels = 2	# s
 seconds_for_reference_frame = 1	# s
 
-
-f = []
-for (dirpath, dirnames, filenames) in os.walk(path):
-	f.append(dirnames)
-days_available = f[0]
-shot_available = []
-for i_day,day in enumerate(to_do):
-	f = []
-	for (dirpath, dirnames, filenames) in os.walk(path+day+'/'):
-		f.append(filenames)
-	shot_available.append([])
-	for name in f[0]:
-		if name[-3:]=='ats' or name[-3:]=='ptw':
-			shot_available[i_day].append(name)
-
-
 every_pixel_independent = False
 
-# for i_day,day in enumerate(to_do):
-# # for i_day,day in enumerate(np.flip(to_do,axis=0)):
-# 	# for name in shot_available[i_day]:
-# 	for name in np.flip(shot_available[i_day],axis=0):
-# 		laser_to_analyse=path+day+'/'+name
-#
-# 		exec(open("/home/ffederic/work/analysis_scripts/scripts/MASTU_pulse_process.py").read())
 
-i_day,day = 0,'2021-07-01'
-name='IRVB-MASTU_shot-44391.ptw'
-# i_day,day = 0,'2021-07-06'
-# name='IRVB-MASTU_shot-44415.ptw'
-laser_to_analyse=path+day+'/'+name
-exec(open("/home/ffederic/work/analysis_scripts/scripts/MASTU_pulse_process2.py").read())
-exec(open("/home/ffederic/work/analysis_scripts/scripts/MASTU_temp_to_power3.py").read())
+last_pulse_path = '/home/ffederic/work/irvb/MAST-U/'
+last_pulse_dict = dict(np.load(last_pulse_path+'last_pulse.npz'))
+
+day = str(last_pulse_dict['location'][0])
+if len(last_pulse_dict['location'])>1:
+	last_pulse_dict['location'] = last_pulse_dict['location'][1:]
+elif len(last_pulse_dict['location'])==0:
+	print('no pulse to analyse')
+	exit()
+else:
+	last_pulse_dict['location'] = []
+name = str(last_pulse_dict['filename'][0])
+if len(last_pulse_dict['filename'])>1:
+	last_pulse_dict['filename'] = last_pulse_dict['filename'][1:]
+else:
+	last_pulse_dict['filename'] = []
+np.savez_compressed(last_pulse_path+'last_pulse',**last_pulse_dict)
+
+laser_to_analyse=last_pulse_path+day+'/'+name
+path = last_pulse_path
+
+exec(open("/home/ffederic/work/analysis_scripts/scripts/MASTU_pulse_process.py").read())