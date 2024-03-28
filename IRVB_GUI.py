perfected the inversion with no emission fixed on the pinhole, better calculation of the uncertainly and corrected geometry matrix to account for the real position of the pinhole instead of using the nominal one from design

IRVB_GUI.py
@@ -327,6 +327,7 @@ params = [
 		{'name': 'Export down', 'type': 'float', 'value': 10, 'step': 1e-3, 'finite': False},
 		{'name': 'Include prelude', 'type': 'bool', 'value': True, 'tip': "This is a checkbox"},
 		{'name': 'Export size', 'type': 'float', 'value': 15, 'step': 1e-3, 'finite': False},
+		{'name': 'Color map', 'type': 'str', 'value': "rainbow", 'tip': 'try'},
 	]},
 	{'name': 'Overlays', 'type': 'group', 'children': [
 		{'name': 'Structure', 'type': 'bool', 'value': True, 'tip': "This is a checkbox"},
@@ -575,10 +576,10 @@ def export_video():
 	if flag_radial_profile:
 		extent = [inversion_R.min()-dr/2, inversion_R.max()+dr/2, inversion_Z.min()-dz/2, inversion_Z.max()+dz/2]
 		image_extent = [full_range_hor[limit_left], full_range_hor[limit_right], full_range_ver[limit_bottom], full_range_ver[limit_top]]
-		ani = coleval.movie_from_data_radial_profile(np.array([np.transpose(np.flip(data,axis=1)[start_time:end_time],(0,2,1))]), framerate,timesteps=time_array[start_time:end_time],integration=2,time_offset=time_array[start_time],extvmin=histogram_level_low_for_plot,extvmax=histogram_level_high_for_plot, extent = extent, image_extent=image_extent,xlabel='R [m]', ylabel='Z [m]',barlabel=barlabel, prelude='shot '  + w2.shotID + '\n'+str(param_ext['Set display','Binning'])+'\n'+'grid resolution %.3gcm\n' %(int(param_ext['Set display','Voxel res'])) ,overlay_structure=param_ext['Overlays','Structure'],include_EFIT=True,efit_reconstruction=efit_reconstruction,pulse_ID=w2.shotID,overlay_x_point=param_ext['Overlays','X-point'],overlay_mag_axis=param_ext['Overlays','Mag axis'],overlay_strike_points=param_ext['Overlays','Separatrix'],overlay_separatrix=param_ext['Overlays','Separatrix'],overlay_res_bolo=param_ext['Overlays','Core Resistive bol'] or param_ext['Overlays','Div Resistive bol'])#,extvmin=0,extvmax=4e4)
+		ani = coleval.movie_from_data_radial_profile(np.array([np.transpose(np.flip(data,axis=1)[start_time:end_time],(0,2,1))]), framerate,timesteps=time_array[start_time:end_time],integration=2,time_offset=time_array[start_time],extvmin=histogram_level_low_for_plot,extvmax=histogram_level_high_for_plot, extent = extent, image_extent=image_extent,xlabel='R [m]', ylabel='Z [m]',barlabel=barlabel, prelude='shot '  + w2.shotID + '\n'+str(param_ext['Set display','Binning'])+'\n'+'grid resolution %.3gcm\n' %(int(param_ext['Set display','Voxel res'])) ,overlay_structure=param_ext['Overlays','Structure'],include_EFIT=True,efit_reconstruction=efit_reconstruction,pulse_ID=w2.shotID,overlay_x_point=param_ext['Overlays','X-point'],overlay_mag_axis=param_ext['Overlays','Mag axis'],overlay_strike_points=param_ext['Overlays','Separatrix'],overlay_separatrix=param_ext['Overlays','Separatrix'],overlay_res_bolo=param_ext['Overlays','Core Resistive bol'] or param_ext['Overlays','Div Resistive bol'],cmap=param_ext['Set display', 'Color map'])#,extvmin=0,extvmax=4e4)
 	else:
 		image_extent = [full_range_hor[limit_left]-0.5, full_range_hor[limit_right]-1+0.5, full_range_ver[limit_bottom]-0.5, full_range_ver[limit_top]-1+0.5]
-		ani = coleval.movie_from_data(np.array([np.flip(np.transpose(np.flip(data,axis=1)[start_time:end_time],(0,2,1)),axis=2)]), framerate,image_extent=image_extent,timesteps=time_array[start_time:end_time],integration=2,time_offset=time_array[start_time],extvmin=histogram_level_low_for_plot,extvmax=histogram_level_high_for_plot,xlabel='horizontal coord [pixels]', ylabel='vertical coord [pixels]',barlabel=barlabel, prelude='shot ' + w2.shotID + '\n'+str(param_ext['Set display','Binning'])+'\n',overlay_structure=param_ext['Overlays','Structure'],include_EFIT=True,efit_reconstruction=efit_reconstruction,pulse_ID=w2.shotID,overlay_x_point=param_ext['Overlays','X-point'],overlay_mag_axis=param_ext['Overlays','Mag axis'],overlay_strike_points=param_ext['Overlays','Separatrix'],overlay_separatrix=param_ext['Overlays','Separatrix'],overlay_res_bolo=param_ext['Overlays','Core Resistive bol'] or param_ext['Overlays','Div Resistive bol'])
+		ani = coleval.movie_from_data(np.array([np.flip(np.transpose(np.flip(data,axis=1)[start_time:end_time],(0,2,1)),axis=2)]), framerate,image_extent=image_extent,timesteps=time_array[start_time:end_time],integration=2,time_offset=time_array[start_time],extvmin=histogram_level_low_for_plot,extvmax=histogram_level_high_for_plot,xlabel='horizontal coord [pixels]', ylabel='vertical coord [pixels]',barlabel=barlabel, prelude='shot ' + w2.shotID + '\n'+str(param_ext['Set display','Binning'])+'\n',overlay_structure=param_ext['Overlays','Structure'],include_EFIT=True,efit_reconstruction=efit_reconstruction,pulse_ID=w2.shotID,overlay_x_point=param_ext['Overlays','X-point'],overlay_mag_axis=param_ext['Overlays','Mag axis'],overlay_strike_points=param_ext['Overlays','Separatrix'],overlay_separatrix=param_ext['Overlays','Separatrix'],overlay_res_bolo=param_ext['Overlays','Core Resistive bol'] or param_ext['Overlays','Div Resistive bol'],cmap=param_ext['Set display', 'Color map'])
 	ani.save(param_ext1['File Path'] + '_export_' + str(next_export) + '.mp4', fps=5*framerate/383, writer='ffmpeg',codec='mpeg4')
 	plt.close('all')
 	print('\n'+'\n'+param_ext1['File Path'] + '_export_' + str(next_export) + '.mp4 generated'+'\n'+'\n')
@@ -645,7 +646,7 @@ def export_image():
 			prelude = ''
 		extent = [inversion_R.min()-dr/2, inversion_R.max()+dr/2, inversion_Z.min()-dz/2, inversion_Z.max()+dz/2]
 		image_extent = [full_range_hor[limit_left], full_range_hor[limit_right], full_range_ver[limit_bottom], full_range_ver[limit_top]]
-		fig,efit_reconstruction = coleval.image_from_data_radial_profile(np.array([np.transpose(np.flip([to_plot],axis=1),(0,2,1))]),form_factor_size=param_ext['Set display','Export size'],ref_time=param_ext['ROI', 'Time [ms]']*1e-3,extvmin=histogram_level_low_for_plot,extvmax=histogram_level_high_for_plot, extent = extent, image_extent=image_extent,xlabel='R [m]', ylabel='Z [m]',barlabel=barlabel, prelude=prelude ,overlay_structure=param_ext['Overlays','Structure'],include_EFIT=include_EFIT,efit_reconstruction=efit_reconstruction,pulse_ID=w2.shotID,overlay_x_point=param_ext['Overlays','X-point'],overlay_mag_axis=param_ext['Overlays','Mag axis'],overlay_strike_points=param_ext['Overlays','Separatrix'],overlay_separatrix=param_ext['Overlays','Separatrix'],overlay_res_bolo=param_ext['Overlays','Core Resistive bol'] or param_ext['Overlays','Div Resistive bol'],EFIT_output_requested=True)#,extvmin=0,extvmax=4e4)
+		fig,efit_reconstruction = coleval.image_from_data_radial_profile(np.array([np.transpose(np.flip([to_plot],axis=1),(0,2,1))]),form_factor_size=param_ext['Set display','Export size'],ref_time=param_ext['ROI', 'Time [ms]']*1e-3,extvmin=histogram_level_low_for_plot,extvmax=histogram_level_high_for_plot, extent = extent, image_extent=image_extent,xlabel='R [m]', ylabel='Z [m]',barlabel=barlabel, prelude=prelude ,overlay_structure=param_ext['Overlays','Structure'],include_EFIT=include_EFIT,efit_reconstruction=efit_reconstruction,pulse_ID=w2.shotID,overlay_x_point=param_ext['Overlays','X-point'],overlay_mag_axis=param_ext['Overlays','Mag axis'],overlay_strike_points=param_ext['Overlays','Separatrix'],overlay_separatrix=param_ext['Overlays','Separatrix'],overlay_res_bolo=param_ext['Overlays','Core Resistive bol'] or param_ext['Overlays','Div Resistive bol'],EFIT_output_requested=True,cmap=param_ext['Set display', 'Color map'])#,extvmin=0,extvmax=4e4)
 	else:
 		if param_ext['Set display','Include prelude']:
 			prelude = 'shot ' + w2.shotID + '\n'+str(param_ext['Set display','Binning'])+'\n'+'Time %.3gms\n' %(param_ext['ROI', 'Time [ms]'])
@@ -1573,43 +1574,67 @@ elif False:	# plots required for the paper/thesis, shot 45295
 
 elif False:	# section to find the orientation of IRVB in MU02/3/4
 
-	origin_HM04A = np.array([2.105,-0.372,-0.002])	# m
-	# direction_HM04A = np.array([-0.890,0.409,-0.204])	# m
-	target_HM04A = np.array([-1.524,1.295,-0.833])	# m
-	direction_HM04A = target_HM04A-origin_HM04A
-	fit_uncertainty_HM04A = 12.7	# pixels
+	if False:	# geometry calculated using the central column in calcam. This might be bad as the CC was demonstrated to be shifted down by 6mm
+		origin_HM04A = np.array([2.105,-0.372,-0.002])	# m
+		# direction_HM04A = np.array([-0.890,0.409,-0.204])	# m
+		target_HM04A = np.array([-1.524,1.295,-0.833])	# m
+		direction_HM04A = target_HM04A-origin_HM04A
+		fit_uncertainty_HM04A = 12.7	# pixels
 
-	origin_HM03D = np.array([2.166,0.370,-0.001])	# m
-	# direction_HM03D = np.array([-0.954,0.208,-0.214])	# m
-	target_HM03D = np.array([-1.605,1.193,-0.846])	# m
-	direction_HM03D = target_HM03D-origin_HM03D
-	fit_uncertainty_HM03D = 4.8	# pixels
+		origin_HM03D = np.array([2.166,0.370,-0.001])	# m
+		# direction_HM03D = np.array([-0.954,0.208,-0.214])	# m
+		target_HM03D = np.array([-1.605,1.193,-0.846])	# m
+		direction_HM03D = target_HM03D-origin_HM03D
+		fit_uncertainty_HM03D = 4.8	# pixels
 
-	origin_HM07C = np.array([-0.541,-2.118,0.200])	# m
-	# direction_HM07C = np.array([-0.149,0.950,-0.273])	# m
-	target_HM07C = np.array([-1.137,1.670,-0.889])	# m
-	direction_HM07C = target_HM07C-origin_HM07C
-	fit_uncertainty_HM07C = 4.7	# pixels
+		origin_HM07C = np.array([-0.541,-2.118,0.200])	# m
+		# direction_HM07C = np.array([-0.149,0.950,-0.273])	# m
+		target_HM07C = np.array([-1.137,1.670,-0.889])	# m
+		direction_HM07C = target_HM07C-origin_HM07C
+		fit_uncertainty_HM07C = 4.7	# pixels
 
-	origin_HM08A = np.array([-1.274,-1.150,0.001])	# m
-	# direction_HM08A = np.array([0.099,0.947,-0.306])	# m
-	target_HM08A = np.array([-0.972,1.748,-0.935])	# m
-	direction_HM08A = target_HM08A-origin_HM08A
-	fit_uncertainty_HM08A = 6.5	# pixels
+		origin_HM08A = np.array([-1.274,-1.150,0.001])	# m
+		# direction_HM08A = np.array([0.099,0.947,-0.306])	# m
+		target_HM08A = np.array([-0.972,1.748,-0.935])	# m
+		direction_HM08A = target_HM08A-origin_HM08A
+		fit_uncertainty_HM08A = 6.5	# pixels
 
-	origin_HM08B = np.array([-1.080,-1.325,0.061])	# m
-	# direction_HM08B = np.array([0.015,0.951,-0.308])	# m
-	target_HM08B = np.array([-1.031,1.679,-0.913])	# m
-	direction_HM08B = target_HM08B-origin_HM08B
-	fit_uncertainty_HM08B = 5.4	# pixels
+		origin_HM08B = np.array([-1.080,-1.325,0.061])	# m
+		# direction_HM08B = np.array([0.015,0.951,-0.308])	# m
+		target_HM08B = np.array([-1.031,1.679,-0.913])	# m
+		direction_HM08B = target_HM08B-origin_HM08B
+		fit_uncertainty_HM08B = 5.4	# pixels
 
-	ax = plt.figure().add_subplot(projection='3d')
-	ax.plot([origin_HM04A[0],origin_HM04A[0]+direction_HM04A[0]],[origin_HM04A[1],origin_HM04A[1]+direction_HM04A[1]],[origin_HM04A[2],origin_HM04A[2]+direction_HM04A[2]])
-	ax.plot([origin_HM03D[0],origin_HM03D[0]+direction_HM03D[0]],[origin_HM03D[1],origin_HM03D[1]+direction_HM03D[1]],[origin_HM03D[2],origin_HM03D[2]+direction_HM03D[2]])
-	ax.plot([origin_HM07C[0],origin_HM07C[0]+direction_HM07C[0]],[origin_HM07C[1],origin_HM07C[1]+direction_HM07C[1]],[origin_HM07C[2],origin_HM07C[2]+direction_HM07C[2]])
-	ax.plot([origin_HM08A[0],origin_HM08A[0]+direction_HM08A[0]],[origin_HM08A[1],origin_HM08A[1]+direction_HM08A[1]],[origin_HM08A[2],origin_HM08A[2]+direction_HM08A[2]])
-	ax.plot([origin_HM08B[0],origin_HM08B[0]+direction_HM08B[0]],[origin_HM08B[1],origin_HM08B[1]+direction_HM08B[1]],[origin_HM08B[2],origin_HM08B[2]+direction_HM08B[2]])
-	ax.plot(coleval.locate_pinhole()[0],coleval.locate_pinhole()[1],coleval.locate_pinhole()[2],'o')
+	else:	# this is calculated NOT using the central column
+		origin_HM04A = np.array([2.105,-0.372,-0.002])	# m
+		# direction_HM04A = np.array([-0.890,0.409,-0.204])	# m
+		target_HM04A = np.array([-1.524,1.295,-0.833])	# m
+		direction_HM04A = target_HM04A-origin_HM04A
+		fit_uncertainty_HM04A = 12.7	# pixels
+
+		origin_HM03D = np.array([2.172,0.365,-0.027])	# m
+		# direction_HM03D = np.array([-0.954,0.208,-0.214])	# m
+		target_HM03D = np.array([-1.604,1.194,-0.839])	# m
+		direction_HM03D = target_HM03D-origin_HM03D
+		fit_uncertainty_HM03D = 5.1	# pixels
+
+		origin_HM07C = np.array([-0.557,-2.112,0.190])	# m
+		# direction_HM07C = np.array([-0.149,0.950,-0.273])	# m
+		target_HM07C = np.array([-1.133,1.672,-0.888])	# m
+		direction_HM07C = target_HM07C-origin_HM07C
+		fit_uncertainty_HM07C = 5.4	# pixels
+
+		origin_HM08A = np.array([-1.283,-1.191,-0.008])	# m
+		# direction_HM08A = np.array([0.099,0.947,-0.306])	# m
+		target_HM08A = np.array([-0.972,1.721,-0.920])	# m
+		direction_HM08A = target_HM08A-origin_HM08A
+		fit_uncertainty_HM08A = 6.3	# pixels
+
+		origin_HM08B = np.array([-1.103,-1.369,0.047])	# m
+		# direction_HM08B = np.array([0.015,0.951,-0.308])	# m
+		target_HM08B = np.array([-1.024,1.741,-0.925])	# m
+		direction_HM08B = target_HM08B-origin_HM08B
+		fit_uncertainty_HM08B = 5.1	# pixels
 
 	# Define the lines in terms of their origin, direction, and uncertainty
 	lines = [
@@ -1643,13 +1668,21 @@ elif False:	# section to find the orientation of IRVB in MU02/3/4
 		return distance
 
 	bds = [[coleval.locate_pinhole()[0]-0.2,coleval.locate_pinhole()[1]-0.2,coleval.locate_pinhole()[2]-0.2],[coleval.locate_pinhole()[0]+0.2,coleval.locate_pinhole()[1]+0.2,coleval.locate_pinhole()[2]+0.2]]
-	found_point = curve_fit(calculate_error_lines,[0,0,0,0,0],[0,0,0,0,0],p0=coleval.locate_pinhole(),bounds=bds,ftol=1e-12)[0]
-	ax.plot(found_point[0],found_point[1],found_point[2],'+')
+	found_point = curve_fit(calculate_error_lines,[0,0,0,0,0],[0,0,0,0,0],p0=coleval.locate_pinhole(),bounds=bds,ftol=1e-12,xtol=1e-10)
+	found_point = found_point[0]
 	found_point-coleval.locate_pinhole()
+
+	ax = plt.figure().add_subplot(projection='3d')
+	ax.plot([origin_HM04A[0],origin_HM04A[0]+direction_HM04A[0]],[origin_HM04A[1],origin_HM04A[1]+direction_HM04A[1]],[origin_HM04A[2],origin_HM04A[2]+direction_HM04A[2]])
+	ax.plot([origin_HM03D[0],origin_HM03D[0]+direction_HM03D[0]],[origin_HM03D[1],origin_HM03D[1]+direction_HM03D[1]],[origin_HM03D[2],origin_HM03D[2]+direction_HM03D[2]])
+	ax.plot([origin_HM07C[0],origin_HM07C[0]+direction_HM07C[0]],[origin_HM07C[1],origin_HM07C[1]+direction_HM07C[1]],[origin_HM07C[2],origin_HM07C[2]+direction_HM07C[2]])
+	ax.plot([origin_HM08A[0],origin_HM08A[0]+direction_HM08A[0]],[origin_HM08A[1],origin_HM08A[1]+direction_HM08A[1]],[origin_HM08A[2],origin_HM08A[2]+direction_HM08A[2]])
+	ax.plot([origin_HM08B[0],origin_HM08B[0]+direction_HM08B[0]],[origin_HM08B[1],origin_HM08B[1]+direction_HM08B[1]],[origin_HM08B[2],origin_HM08B[2]+direction_HM08B[2]])
+	ax.plot(coleval.locate_pinhole()[0],coleval.locate_pinhole()[1],coleval.locate_pinhole()[2],'o')
+	ax.plot(found_point[0],found_point[1],found_point[2],'+')
 
 
 	flange_location = np.array([[-1451.46052984755,1544.4989953858,-795.127973818281],[-1445.66263688161,1550.31177880053,-789.148486275245],[-1430.18747663838,1565.87180245998,-630.295559614074],[-1434.78339642903,1561.26804243527,-622.483810832221],[-1527.59526499331,1468.2591265621,-587.351682983045],[-1534.30001951375,1461.53816453532,-591.061454533504],[-1568.1921106691,1427.51401088464,-768.590696880916],[-1563.91726826618,1431.79586049244,-776.760250709844],[-1451.46052984755,1544.4989953858,-795.127973818281],[-1451.46052984755,1544.4989953858,-795.1279738182813]])/1000
-	ax.plot(flange_location[:,0],flange_location[:,1],flange_location[:,2],'+')
 
 	def calculate_error_circle(x,point_0,point_1,point_2,dir_0,dir_1,dir_2,radius):
 		center = np.array([point_0,point_1,point_2])
@@ -1673,7 +1706,7 @@ elif False:	# section to find the orientation of IRVB in MU02/3/4
 
 	bds = [[-np.inf,-np.inf,-np.inf,-1,-1,-1,-np.inf],[np.inf,np.inf,np.inf,1,1,1,np.inf]]
 	gna = curve_fit(calculate_error_circle,[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],p0=(np.mean(flange_location,axis=0)[0],np.mean(flange_location,axis=0)[1],np.mean(flange_location,axis=0)[2],1,-1,0,0.1),maxfev=100000,bounds=bds,xtol=1e-15)[0]
-	gna = np.array([-1.50043085e+00,  1.49546247e+00, -6.99230699e-01,  9.99998644e-01,-9.99033623e-01, -1.96367575e-04,  1.18317306e-01])
+	# gna = np.array([-1.50043085e+00,  1.49546247e+00, -6.99230699e-01,  9.99998644e-01,-9.99033623e-01, -1.96367575e-04,  1.18317306e-01])
 
 	# how to plot a circle
 	center = cp.deepcopy(gna[:3])
@@ -1707,10 +1740,19 @@ elif False:	# section to find the orientation of IRVB in MU02/3/4
 	# Translate the circle to the specified center
 	circle_3d += np.array(center)[:, np.newaxis]
 
+	ax.plot(flange_location[:,0],flange_location[:,1],flange_location[:,2],'+')
 	ax.plot(circle_3d[0],circle_3d[1],circle_3d[2])
 	ax.plot([center[0],center[0]+direction[0]*2],[center[1],center[1]+direction[1]*2],[center[2],center[2]+direction[2]*2])
 
-	centre_of_IRVB_plate = np.array([-1.05649,1.05731,-0.69705])
+	if False:	# geometry calculated using the central column in calcam. This might be bad as the CC was demonstrated to be shifted down by 6mm
+		centre_of_IRVB_plate = np.array([-1.05649,1.05731,-0.69705])
+	else:	# this is calculated NOT using the central column
+		centre_of_IRVB_plate = np.array([-1.0556,1.057,-0.69693])
+
+	if True:	# I add 1/2mm to the left, to see if I can remove radiation from the central column
+		found_point += np.array([0.001,0.001,0])*1
+		centre_of_IRVB_plate += np.array([0.001,0.001,0])*1
+
 
 	stand_off_length = 0.06	# m
 	# Rf=1.54967	# m	radius of the centre of the foil
@@ -1735,6 +1777,8 @@ elif False:	# section to find the orientation of IRVB in MU02/3/4
 	structure_point_location_on_foil = coleval.return_structure_point_location_on_foil(plane_equation=plane_equation,centre_of_foil=centre_of_foil)
 	for i in range(len(structure_point_location_on_foil)):
 		ax.plot(np.array(structure_point_location_on_foil[i][:,0])*(np.shape(cv0)[1]-1)/foil_size[0],np.array(structure_point_location_on_foil[i][:,1])*(np.shape(cv0)[0]-1)/foil_size[1],'--b',alpha=structure_alpha)
+	plt.axis('equal')
+	plt.plot([0,cv0.shape[1],cv0.shape[1],0,0],[0,0,cv0.shape[0],cv0.shape[0],0],'k')
 
 
 

Laser_data_analysis3_3.py
@@ -1807,13 +1807,14 @@ if False:	# here I try to estimate thickness_over_diffusivity from an analytic f
 else:	# here I want to fit thickness_over_diffusivity from the peak time derivative alone
 	nuc_plate_emissivity = 1.0
 	reference_temperature_correction = -0.0
+	nuc_plate_emissivity_range = np.arange(0.5,1.65,0.1)
 	# aggregated_emissivity_range = np.array(np.linspace(3.5,1.5,6).tolist() + np.linspace(1.2,0.3,10).tolist())
 	from scipy.signal import savgol_filter
 
 	collect_collect_thickness_over_diffusivity_from_peak_match = []
 	collect_collect_real_thermaldiffusivity = []
 	collect_full_error=[]
-	for nuc_plate_emissivity in np.arange(0.5,1.65,0.1):
+	for nuc_plate_emissivity in nuc_plate_emissivity_range:
 
 
 		plt.figure()
@@ -2304,6 +2305,8 @@ else:	# here I want to fit thickness_over_diffusivity from the peak time derivat
 
 	thickness_over_diffusivity_per_NUC = []
 	thickness_over_diffusivity_per_NUC_std = []
+	thickness_per_NUC = []
+	thickness_per_NUC_std = []
 	thermaldiffusivity_per_NUC = []
 	thermaldiffusivity_per_NUC_std = []
 	emissivity_per_NUC = []
@@ -2311,10 +2314,11 @@ else:	# here I want to fit thickness_over_diffusivity from the peak time derivat
 	reference_temperature = []
 	reference_temperature_std = []
 	from scipy.interpolate import LinearNDInterpolator,CloughTocher2DInterpolator
-	for i_,nuc_plate_emissivity in enumerate(np.arange(0.5,1.65,0.1)):
+	for i_,nuc_plate_emissivity in enumerate(nuc_plate_emissivity_range):
 
 		full_error_interpolator = CloughTocher2DInterpolator(list(zip(collect_aggregated_emissivity*nuc_plate_emissivity,collect_reference_temperature)),collect_full_error[i_])
 		thickness_over_diffusivity_from_peak_match_interpolator = CloughTocher2DInterpolator(list(zip(collect_aggregated_emissivity*nuc_plate_emissivity,collect_reference_temperature)),collect_collect_thickness_over_diffusivity_from_peak_match[i_])
+		thickness_from_peak_match_interpolator = CloughTocher2DInterpolator(list(zip(collect_aggregated_emissivity*nuc_plate_emissivity,collect_reference_temperature)),collect_collect_thickness_over_diffusivity_from_peak_match[i_]*collect_collect_real_thermaldiffusivity[i_])
 		thermaldiffusivity_interpolator = CloughTocher2DInterpolator(list(zip(collect_aggregated_emissivity*nuc_plate_emissivity,collect_reference_temperature)),collect_collect_real_thermaldiffusivity[i_])
 		emissivity_ = np.linspace(min(collect_aggregated_emissivity*nuc_plate_emissivity),max(collect_aggregated_emissivity*nuc_plate_emissivity),num=100)
 		reference_temperature_ = np.linspace(min(collect_reference_temperature),max(collect_reference_temperature),num=100)
@@ -2343,6 +2347,8 @@ else:	# here I want to fit thickness_over_diffusivity from the peak time derivat
 
 		thickness_over_diffusivity_per_NUC.append(np.sum((thickness_over_diffusivity_from_peak_match_interpolator(emissivity,reference_temperature__)/full_error)[select])/np.sum(1/full_error[select]))
 		thickness_over_diffusivity_per_NUC_std.append(np.std((thickness_over_diffusivity_from_peak_match_interpolator(emissivity,reference_temperature__)/full_error)[select])/np.mean(1/full_error[select]))
+		thickness_per_NUC.append(np.sum((thickness_from_peak_match_interpolator(emissivity,reference_temperature__)/full_error)[select])/np.sum(1/full_error[select]))
+		thickness_per_NUC_std.append(np.std((thickness_from_peak_match_interpolator(emissivity,reference_temperature__)/full_error)[select])/np.mean(1/full_error[select]))
 		thermaldiffusivity_per_NUC.append(np.sum((thermaldiffusivity_interpolator(emissivity,reference_temperature__)/full_error)[select])/np.sum(1/full_error[select]))
 		thermaldiffusivity_per_NUC_std.append(np.std((thermaldiffusivity_interpolator(emissivity,reference_temperature__)/full_error)[select])/np.mean(1/full_error[select]))
 		emissivity_per_NUC.append(np.sum((emissivity/full_error)[select])/np.sum(1/full_error[select]))
@@ -2352,29 +2358,37 @@ else:	# here I want to fit thickness_over_diffusivity from the peak time derivat
 
 
 	plt.figure()
-	plt.errorbar(np.arange(0.5,1.65,0.1),thickness_over_diffusivity_per_NUC,yerr=thickness_over_diffusivity_per_NUC_std,label='thickness_over_diffusivity_per_NUC')
-	fit = np.polyfit(np.arange(0.5,1.65,0.1),thickness_over_diffusivity_per_NUC,2,w=1/np.array(thickness_over_diffusivity_per_NUC_std))
-	plt.plot(np.arange(0.5,1.65,0.1),np.polyval(fit,np.arange(0.5,1.65,0.1)),'--')
+	plt.errorbar(nuc_plate_emissivity_range,thickness_over_diffusivity_per_NUC,yerr=thickness_over_diffusivity_per_NUC_std,label='thickness_over_diffusivity_per_NUC')
+	fit = np.polyfit(nuc_plate_emissivity_range,thickness_over_diffusivity_per_NUC,2,w=1/np.array(thickness_over_diffusivity_per_NUC_std))
+	plt.plot(nuc_plate_emissivity_range,np.polyval(fit,nuc_plate_emissivity_range),'--')
 	plt.grid()
 	plt.xlabel('NUC emissivity')
 	plt.ylabel('thickness_over_diffusivity')
 	plt.title('nuc_plate_emissivity '+str(nuc_plate_emissivity) + '\nlaser power %.3gW' %(laser_to_analyse_power))
 	plt.figure()
-	plt.errorbar(np.arange(0.5,1.65,0.1),thermaldiffusivity_per_NUC,yerr=thermaldiffusivity_per_NUC_std,label='thermaldiffusivity_per_NUC')
+	plt.errorbar(nuc_plate_emissivity_range,thickness_per_NUC,yerr=thickness_per_NUC_std,label='thickness_per_NUC')
+	fit = np.polyfit(nuc_plate_emissivity_range,thickness_per_NUC,2,w=1/np.array(thickness_per_NUC_std))
+	plt.plot(nuc_plate_emissivity_range,np.polyval(fit,nuc_plate_emissivity_range),'--')
+	plt.grid()
+	plt.xlabel('NUC emissivity')
+	plt.ylabel('thickness')
+	plt.title('nuc_plate_emissivity '+str(nuc_plate_emissivity) + '\nlaser power %.3gW' %(laser_to_analyse_power))
+	plt.figure()
+	plt.errorbar(nuc_plate_emissivity_range,thermaldiffusivity_per_NUC,yerr=thermaldiffusivity_per_NUC_std,label='thermaldiffusivity_per_NUC')
 	plt.title('nuc_plate_emissivity '+str(nuc_plate_emissivity) + '\nlaser power %.3gW' %(laser_to_analyse_power))
 	plt.grid()
 	plt.xlabel('NUC emissivity')
 	plt.ylabel('thermaldiffusivity')
 	plt.figure()
-	plt.errorbar(np.arange(0.5,1.65,0.1),emissivity_per_NUC,yerr=emissivity_per_NUC_std,label='emissivity_per_NUC')
-	fit = np.polyfit(np.arange(0.5,1.65,0.1),emissivity_per_NUC,2,w=1/np.array(thermaldiffusivity_per_NUC_std))
-	plt.plot(np.arange(0.5,1.65,0.1),np.polyval(fit,np.arange(0.5,1.65,0.1)),'--')
+	plt.errorbar(nuc_plate_emissivity_range,emissivity_per_NUC,yerr=emissivity_per_NUC_std,label='emissivity_per_NUC')
+	fit = np.polyfit(nuc_plate_emissivity_range,emissivity_per_NUC,2,w=1/np.array(thermaldiffusivity_per_NUC_std))
+	plt.plot(nuc_plate_emissivity_range,np.polyval(fit,nuc_plate_emissivity_range),'--')
 	plt.grid()
 	plt.xlabel('NUC emissivity')
 	plt.ylabel('emissivity')
 	plt.title('nuc_plate_emissivity '+str(nuc_plate_emissivity) + '\nlaser power %.3gW' %(laser_to_analyse_power))
 	plt.figure()
-	plt.errorbar(np.arange(0.5,1.65,0.1),reference_temperature,yerr=reference_temperature_std,label='reference_temperature')
+	plt.errorbar(nuc_plate_emissivity_range,reference_temperature,yerr=reference_temperature_std,label='reference_temperature')
 	plt.title('nuc_plate_emissivity '+str(nuc_plate_emissivity) + '\nlaser power %.3gW' %(laser_to_analyse_power))
 	plt.grid()
 	plt.xlabel('NUC emissivity')
@@ -2438,7 +2452,7 @@ else:	# here I want to fit thickness_over_diffusivity from the peak time derivat
 
 		collect_large_error_fully_defocused = []
 		collect_full_error_fully_defocused = []
-		for i_nuc_plate_emissivity,nuc_plate_emissivity in enumerate(np.arange(0.5,1.65,0.1)):
+		for i_nuc_plate_emissivity,nuc_plate_emissivity in enumerate(nuc_plate_emissivity_range):
 			plt.figure()
 			collect_high_level_std = []
 			collect_high_level_std_small = []
@@ -2608,7 +2622,7 @@ else:	# here I want to fit thickness_over_diffusivity from the peak time derivat
 		reference_temperature_fully_defocused = []
 		reference_temperature_std_fully_defocused = []
 		from scipy.interpolate import LinearNDInterpolator,CloughTocher2DInterpolator
-		for i_,nuc_plate_emissivity in enumerate(np.arange(0.5,1.65,0.1)):
+		for i_,nuc_plate_emissivity in enumerate(nuc_plate_emissivity_range):
 
 			full_error_interpolator = CloughTocher2DInterpolator(list(zip(collect_aggregated_emissivity*nuc_plate_emissivity,collect_reference_temperature)),collect_full_error_fully_defocused[i_])
 			thickness_over_diffusivity_from_peak_match_interpolator = CloughTocher2DInterpolator(list(zip(collect_aggregated_emissivity*nuc_plate_emissivity,collect_reference_temperature)),collect_collect_thickness_over_diffusivity_from_peak_match[i_])
@@ -2648,31 +2662,31 @@ else:	# here I want to fit thickness_over_diffusivity from the peak time derivat
 
 
 		plt.figure()
-		plt.errorbar(np.arange(0.5,1.65,0.1),thickness_over_diffusivity_per_NUC_fully_defocused,yerr=thickness_over_diffusivity_per_NUC_std_fully_defocused,label='thickness_over_diffusivity_per_NUC_fully_defocused')
-		fit = np.polyfit(np.arange(0.5,1.65,0.1),thickness_over_diffusivity_per_NUC_fully_defocused,2,w=1/np.array(thickness_over_diffusivity_per_NUC_std_fully_defocused))
-		plt.plot(np.arange(0.5,1.65,0.1),np.polyval(fit,np.arange(0.5,1.65,0.1)),'k')
-		plt.errorbar(np.arange(0.5,1.65,0.1),thickness_over_diffusivity_per_NUC,yerr=thickness_over_diffusivity_per_NUC_std,label='thickness_over_diffusivity_per_NUC_fully_defocused',linestyle='--')
-		fit = np.polyfit(np.arange(0.5,1.65,0.1),thickness_over_diffusivity_per_NUC,2,w=1/np.array(thickness_over_diffusivity_per_NUC_std))
-		plt.plot(np.arange(0.5,1.65,0.1),np.polyval(fit,np.arange(0.5,1.65,0.1)),'--k')
+		plt.errorbar(nuc_plate_emissivity_range,thickness_over_diffusivity_per_NUC_fully_defocused,yerr=thickness_over_diffusivity_per_NUC_std_fully_defocused,label='thickness_over_diffusivity_per_NUC_fully_defocused')
+		fit = np.polyfit(nuc_plate_emissivity_range,thickness_over_diffusivity_per_NUC_fully_defocused,2,w=1/np.array(thickness_over_diffusivity_per_NUC_std_fully_defocused))
+		plt.plot(nuc_plate_emissivity_range,np.polyval(fit,nuc_plate_emissivity_range),'k')
+		plt.errorbar(nuc_plate_emissivity_range,thickness_over_diffusivity_per_NUC,yerr=thickness_over_diffusivity_per_NUC_std,label='thickness_over_diffusivity_per_NUC_fully_defocused',linestyle='--')
+		fit = np.polyfit(nuc_plate_emissivity_range,thickness_over_diffusivity_per_NUC,2,w=1/np.array(thickness_over_diffusivity_per_NUC_std))
+		plt.plot(nuc_plate_emissivity_range,np.polyval(fit,nuc_plate_emissivity_range),'--k')
 		plt.grid()
 		plt.xlabel('NUC emissivity')
 		plt.ylabel('thickness_over_diffusivity')
 		plt.title('nuc_plate_emissivity '+str(nuc_plate_emissivity) + '\nlaser power %.3gW' %(laser_to_analyse_power))
 		plt.figure()
-		plt.errorbar(np.arange(0.5,1.65,0.1),thermaldiffusivity_per_NUC_fully_defocused,yerr=thermaldiffusivity_per_NUC_std_fully_defocused,label='thermaldiffusivity_per_NUC_fully_defocused')
+		plt.errorbar(nuc_plate_emissivity_range,thermaldiffusivity_per_NUC_fully_defocused,yerr=thermaldiffusivity_per_NUC_std_fully_defocused,label='thermaldiffusivity_per_NUC_fully_defocused')
 		plt.title('nuc_plate_emissivity '+str(nuc_plate_emissivity) + '\nlaser power %.3gW' %(laser_to_analyse_power))
 		plt.figure()
-		plt.errorbar(np.arange(0.5,1.65,0.1),emissivity_per_NUC_fully_defocused,yerr=emissivity_per_NUC_std_fully_defocused,label='emissivity_per_NUC_fully_defocused')
-		fit = np.polyfit(np.arange(0.5,1.65,0.1),emissivity_per_NUC_fully_defocused,2,w=1/np.array(thermaldiffusivity_per_NUC_std_fully_defocused))
-		plt.plot(np.arange(0.5,1.65,0.1),np.polyval(fit,np.arange(0.5,1.65,0.1)),'--')
-		plt.errorbar(np.arange(0.5,1.65,0.1),emissivity_per_NUC,yerr=emissivity_per_NUC_std,linestyle='--')
-		fit = np.polyfit(np.arange(0.5,1.65,0.1),emissivity_per_NUC,2,w=1/np.array(emissivity_per_NUC_std))
-		plt.plot(np.arange(0.5,1.65,0.1),np.polyval(fit,np.arange(0.5,1.65,0.1)),'--k')
+		plt.errorbar(nuc_plate_emissivity_range,emissivity_per_NUC_fully_defocused,yerr=emissivity_per_NUC_std_fully_defocused,label='emissivity_per_NUC_fully_defocused')
+		fit = np.polyfit(nuc_plate_emissivity_range,emissivity_per_NUC_fully_defocused,2,w=1/np.array(thermaldiffusivity_per_NUC_std_fully_defocused))
+		plt.plot(nuc_plate_emissivity_range,np.polyval(fit,nuc_plate_emissivity_range),'--')
+		plt.errorbar(nuc_plate_emissivity_range,emissivity_per_NUC,yerr=emissivity_per_NUC_std,linestyle='--')
+		fit = np.polyfit(nuc_plate_emissivity_range,emissivity_per_NUC,2,w=1/np.array(emissivity_per_NUC_std))
+		plt.plot(nuc_plate_emissivity_range,np.polyval(fit,nuc_plate_emissivity_range),'--k')
 		plt.xlabel('NUC emissivity')
 		plt.ylabel('emissivity')
 		plt.title('nuc_plate_emissivity '+str(nuc_plate_emissivity) + '\nlaser power %.3gW' %(laser_to_analyse_power))
 		plt.figure()
-		plt.errorbar(np.arange(0.5,1.65,0.1),reference_temperature_fully_defocused,yerr=reference_temperature_std_fully_defocused,label='reference_temperature')
+		plt.errorbar(nuc_plate_emissivity_range,reference_temperature_fully_defocused,yerr=reference_temperature_std_fully_defocused,label='reference_temperature')
 		plt.title('nuc_plate_emissivity '+str(nuc_plate_emissivity) + '\nlaser power %.3gW' %(laser_to_analyse_power))
 	else:	# here i just want to use the found values beforehand to see it it is stuly ok
 

MASTU_manual_plots.py
@@ -24,12 +24,18 @@ try:
 	if pass_number==0:
 		full_saved_file_dict_FAST['first_pass'] = full_saved_file_dict_FAST['first_pass'].all()
 		inverted_dict = full_saved_file_dict_FAST['first_pass']['inverted_dict']
+		powernoback = full_saved_file_dict_FAST['first_pass']['FAST_powernoback']
+		time_binned = full_saved_file_dict_FAST['first_pass']['FAST_time_binned']
 	elif pass_number==1:
 		full_saved_file_dict_FAST['second_pass'] = full_saved_file_dict_FAST['second_pass'].all()
 		inverted_dict = full_saved_file_dict_FAST['second_pass']['inverted_dict']
+		powernoback = full_saved_file_dict_FAST['second_pass']['FAST_powernoback']
+		time_binned = full_saved_file_dict_FAST['second_pass']['FAST_time_binned']
 	else:
 		full_saved_file_dict_FAST['third_pass'] = full_saved_file_dict_FAST['third_pass'].all()
 		inverted_dict = full_saved_file_dict_FAST['third_pass']['inverted_dict']
+		powernoback = full_saved_file_dict_FAST['third_pass']['FAST_powernoback']
+		time_binned = full_saved_file_dict_FAST['third_pass']['FAST_time_binned']
 	grid_resolution = 2	# cm
 	time_full_binned_crop = inverted_dict[str(grid_resolution)]['time_full_binned_crop']
 	inverted_data = inverted_dict[str(grid_resolution)]['inverted_data']
@@ -85,6 +91,10 @@ try:
 
 	filename_root = inverted_dict[str(grid_resolution)]['filename_root']
 	filename_root_add = inverted_dict[str(grid_resolution)]['filename_root_add']
+	# fitted_foil_power = inverted_dict[str(grid_resolution)]['fitted_foil_power']
+	# fitted_foil_power_excluded = inverted_dict[str(grid_resolution)]['fitted_foil_power_excluded']
+	# foil_power = inverted_dict[str(grid_resolution)]['foil_power']
+	# full_foil_power = inverted_dict[str(grid_resolution)]['full_foil_power']
 
 	if True:	# added to deal with the fact that the peak radiation is extremely volatile, and hard to track consistently
 		local_inner_leg_mean_emissivity = np.array(local_inner_leg_mean_emissivity)
@@ -238,7 +248,7 @@ try:
 	full_saved_file_dict_FAST['multi_instrument']['SC'] = SC
 	full_saved_file_dict_FAST['multi_instrument']['SL'] = SL
 
-	plt.figure(figsize=(15, 10))
+	plt.figure(figsize=(30, 10))
 	# plt.errorbar(time_full_binned_crop,outer_leg_tot_rad_power_all/1e3,yerr=outer_leg_tot_rad_power_sigma_all/1e3,label='outer_leg\nwith x-point',capsize=5)
 	plt.errorbar(time_full_binned_crop,outer_leg_reliable_power_all/1e3,yerr=outer_leg_reliable_power_sigma_all/1e3,label='outer_leg\nno x-point\naccurate no sxd',capsize=5)
 	plt.errorbar(time_full_binned_crop,sxd_tot_rad_power_all/1e3,yerr=sxd_tot_rad_power_sigma_all/1e3,label='sxd',capsize=5)
@@ -257,6 +267,7 @@ try:
 	plt.errorbar(time_full_binned_crop,equivalent_res_bolo_view_all/1e3,yerr=equivalent_res_bolo_view_sigma_all/1e3,label='equivalent\nto res bolo',capsize=5,linestyle='--')
 	plt.errorbar(time_full_binned_crop,all_out_of_sxd_all/1e3,yerr=all_out_of_sxd_sigma_all/1e3,label='out sxd',capsize=5,linestyle='--')
 	plt.title('shot ' + laser_to_analyse[-9:-4]+' '+scenario+'\nradiated power in the lower half of the machine')
+	plt.ylim(bottom=0,top=median_filter(all_lower_volume_radiation_all,size=[max(1,len(all_lower_volume_radiation_all)//8)*2+1]).max()/1e3)	# arbitrary limit to see better if there is a disruption at the end of the shot
 	plt.legend(loc='best', fontsize='xx-small')
 	plt.xlabel('time [s]')
 	plt.ylabel('power [kW]')

MASTU_pulse_process2_BB.py
@@ -445,7 +445,7 @@ try:
 		inverted_dict = full_saved_file_dict_FAST['first_pass'].all()['inverted_dict']
 		test = inverted_dict[str(2)]['regolarisation_coeff_all']
 		foilrotdeg,out_of_ROI_mask,foildw,foilup,foillx,foilrx = coleval.get_rotation_crop_parameters(temp_ref_counts[-1],foil_position_dict,laser_to_analyse,laser_counts_corrected[-1],time_of_experiment_digitizer_ID_seconds)
-		print('FAST creastion skipped')
+		print('FAST creation skipped')
 	except:
 		print('generating FAST')
 		try:
@@ -487,7 +487,7 @@ try:
 				override_second_pass_int = override_second_pass
 			except:
 				override_second_pass_int = True
-			foilrotdeg,out_of_ROI_mask,foildw,foilup,foillx,foilrx,FAST_counts_minus_background_crop,time_binned,powernoback,brightness,binning_type,inverted_dict = coleval.MASTU_pulse_process_FAST3_BB(laser_counts_corrected,time_of_experiment_digitizer_ID,time_of_experiment,external_clock_marker,aggregated_correction_coefficients,laser_framerate,laser_digitizer_ID,laser_int_time,seconds_for_reference_frame,start_time_of_pulse,laser_to_analyse,laser_dict['height'],laser_dict['width'],flag_use_of_first_frames_as_reference,params,errparams,params_BB,errparams_BB,photon_flux_over_temperature_interpolator,BB_proportional,BB_proportional_std,foil_position_dict,pass_number = pass_number,x_point_region_radious=0.1,wavewlength_top=5.1,wavelength_bottom=1.5,override_second_pass=override_second_pass_int)
+			foilrotdeg,out_of_ROI_mask,foildw,foilup,foillx,foilrx,FAST_counts_minus_background_crop,time_binned,powernoback,brightness,binning_type,inverted_dict = coleval.MASTU_pulse_process_FAST3_BB(laser_counts_corrected,time_of_experiment_digitizer_ID,time_of_experiment,external_clock_marker,aggregated_correction_coefficients,laser_framerate,laser_digitizer_ID,laser_int_time,seconds_for_reference_frame,start_time_of_pulse,laser_to_analyse,laser_dict['height'],laser_dict['width'],flag_use_of_first_frames_as_reference,params,errparams,params_BB,errparams_BB,photon_flux_over_temperature_interpolator,BB_proportional,BB_proportional_std,foil_position_dict,pass_number = pass_number,x_point_region_radious=0.2,wavewlength_top=5.1,wavelength_bottom=1.5,override_second_pass=override_second_pass_int)
 			full_saved_file_dict_FAST['second_pass'] = dict([])
 			full_saved_file_dict_FAST['second_pass']['FAST_counts_minus_background_crop'] = np.float16(FAST_counts_minus_background_crop)
 			full_saved_file_dict_FAST['second_pass']['FAST_powernoback'] = np.float16(powernoback)

emissivity_to_foil_power.py
@@ -1052,9 +1052,9 @@ if True:	# related to the SOLPS phantom and calculating the spectra produced by 
 	# SOLPS_case = 'ramp_11'
 	# mastu_path = "/home/ffederic/work/SOLPS/dscan/" + SOLPS_case
 
-	use_deuterium_lines = False
-	use_carbon_lines = False
-	use_nitrogen_lines = False
+	use_deuterium_lines = True
+	use_carbon_lines = True
+	use_nitrogen_lines = True
 	use_core = True
 	use_bremmstrahlung = True
 	enables = [use_deuterium_lines,use_carbon_lines,use_nitrogen_lines,use_core,use_bremmstrahlung]
@@ -1063,11 +1063,11 @@ if True:	# related to the SOLPS phantom and calculating the spectra produced by 
 
 	integrated_power = []
 
-	# for i_SOLPS_case,SOLPS_case in enumerate(['seed_1','seed_5','seed_10']):
-	# 	mastu_path = "/home/ffederic/work/SOLPS/seeding/" + SOLPS_case
+	for i_SOLPS_case,SOLPS_case in enumerate(['seed_1','seed_5','seed_10']):
+		mastu_path = "/home/ffederic/work/SOLPS/seeding/" + SOLPS_case
 
-	for i_SOLPS_case,SOLPS_case in enumerate(['ramp_1','ramp_3.3','ramp_11']):
-		mastu_path = "/home/ffederic/work/SOLPS/dscan/" + SOLPS_case
+	# for i_SOLPS_case,SOLPS_case in enumerate(['ramp_1','ramp_3.3','ramp_11']):
+	# 	mastu_path = "/home/ffederic/work/SOLPS/dscan/" + SOLPS_case
 
 		# types = ['midplane','as_up_as_possible','as_up_as_possible_MU04','as_low_as_possible','x_point']
 		types = ['as_up_as_possible_MU04','x_point']	# in reality these are the only one that matter and are usefull
@@ -1085,41 +1085,41 @@ if True:	# related to the SOLPS phantom and calculating the spectra produced by 
 
 
 		if True:
-			plt.figure(2)
+			plt.figure()
 			plt.title('Spectra '+mastu_path[20:]+'\n enables '+str(enables))
 			# plt.plot(spectrum.wavelengths,spectrum.samples)
 			vmin = np.inf
 			for i in range(len(all_spectra)):
 				try:
 					dwave = np.median(np.diff(all_spectra[i].wavelengths))
-					plt.plot(all_spectra[i].wavelengths,all_spectra[i].samples,'--',label=types[i],color='C'+str(i+1))
+					# plt.plot(all_spectra[i].wavelengths,all_spectra[i].samples,'--',label=types[i],color='C'+str(i+1))
 					vmin=min(vmin,all_spectra[i].samples[all_spectra[i].wavelengths.argmax()])
 					print(SOLPS_case+' enables '+str(enables)+''+types[i]+' %.3g W/m2/str' %(np.trapz(all_spectra[i].samples,all_spectra[i].wavelengths)))
 					integrated_power.append(np.trapz(all_spectra[i].samples,all_spectra[i].wavelengths))
 
-					# a=plt.plot(1239.8/all_spectra[i].wavelengths,all_spectra[i].samples,'--',label=types[i])
-					# a=plt.plot(1239.8/all_spectra[i].wavelengths,all_spectra[i].samples*dwave,'--',label=types[i])
+					a=plt.plot(1239.8/all_spectra[i].wavelengths,all_spectra[i].samples,'--',label=types[i])
+					a=plt.plot(1239.8/all_spectra[i].wavelengths,all_spectra[i].samples*dwave,'--',label=types[i])
 					# plt.plot(1239.8/all_spectra[i].wavelengths,all_spectra[i].samples*dwave,'|',color=a[0].get_color())
 					# plt.plot(1239.8/((all_spectra[i].wavelengths[1:]+all_spectra[i].wavelengths[:-1])/2),(all_spectra[i].samples[1:]+all_spectra[i].samples[:-1])/2*dwave/np.abs(np.diff(1239.8/all_spectra[i].wavelengths)),'--',label=types[i])
 				except:
 					pass
-			plt.ylabel('W/m2/sr/nm')
-			plt.xlabel('nm')
-			plt.grid()
-			plt.xlim(left=0,right=2)
-			plt.ylim(bottom=vmin)
-			plt.semilogy()
+			# plt.ylabel('W/m2/sr/nm')
+			# plt.xlabel('nm')
+			# plt.grid()
+			# plt.xlim(left=0,right=2)
+			# plt.ylim(bottom=vmin)
+			# plt.semilogy()
 
 			plt.legend(loc='best', fontsize='xx-small')
-			# plt.grid()
-			# plt.xlabel('eV')
-			# plt.xlim(left=1,right=100000)
-			# plt.ylabel('W/m2/sr')
-			# plt.ylim(bottom=1E-6,top=1e3)
-			# # plt.ylabel('W/m2/sr/eV')
-			# # plt.ylim(bottom=1E-3,top=1e6)
-			# plt.semilogx()
-			# plt.semilogy()
+			plt.grid()
+			plt.xlabel('eV')
+			plt.xlim(left=1,right=100000)
+			plt.ylabel('W/m2/sr')
+			plt.ylim(bottom=1E-6)
+			# plt.ylabel('W/m2/sr/eV')
+			# plt.ylim(bottom=1E-3,top=1e6)
+			plt.semilogx()
+			plt.semilogy()
 			# plt.ylim(bottom=1E-6,top=1e5)
 			# plt.pause(0.01)
 		else: